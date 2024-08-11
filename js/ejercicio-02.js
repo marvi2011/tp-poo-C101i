c82a96baaf0c020cac5eef609504654579a0f0b1
Victoria Gramajo <marvi661@hotmail.com>
2024-08-10 21:03:31 -0300

    correcciones en el ejercicio 2

--- js/ejercicio-02.js
+++ js/ejercicio-02.js
@@ -9,18 +9,6 @@ Utiliza este objeto para mostar la descripcion, ingresar y extraer dinero y vokv
 const cuenta = {
   titular: "Alex",
   saldo: 0,
-<<<<<<< HEAD
-
-  ingresar: function (cantidad) {
-  
-    document.write(`<p>La cantidad ingresada es: ${this.saldo}</p>`);
-  },
-
-  extraer: function () {
-    saldo = resta(saldo, cantidad);
-    document.write(`<p>La cantidad ingresada es: ${this.saldo}</p>`);
-  },
-=======
   ingreso: 100,
   extraccion: 30,
 
@@ -40,17 +28,12 @@ const cuenta = {
     );
   },
 
->>>>>>> ejercicio-03
   informar: function () {
     document.write(`<p>Su nuevo saldo es: ${this.saldo}</p>`);
   },
 };
 //Ahora llamo a los metodos
 console.log(cuenta);
-<<<<<<< HEAD
-document.write(`<p>Cuenta: ${cuenta.ingresar}</p>`);
-document.write(`<p>Cuenta: ${cuenta.extraer}</p>`);
-=======
 document.write(`<h2>Titular de la cuenta: ${cuenta.titular}</h2>`);
 document.write(`<p>Su saldo inicial es: $${cuenta.saldo}</p>`)
 document.write(`<p>Ud ingresó: ${cuenta.ingreso}</p>`);
@@ -58,4 +41,3 @@ let saldoIngreso = cuenta.ingresar();
 document.write(`<p>Su saldo inicial es: $${cuenta.saldo}</p>`)
 document.write(`<p>Ud extrajo: $${cuenta.extraccion}</p>`);
 let saldoExtraccion = cuenta.extraer();
->>>>>>> ejercicio-03
